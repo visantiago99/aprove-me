f7f89106d20a21406bdf2a5f6656c17477ca35e5
Victor Santiago da Cunha <czpask@gmail.com>
2024-12-04 10:27:27 -0300

    Implement jwt secret to .env variables

--- aprove-me-nest/src/auth/auth.module.ts
+++ aprove-me-nest/src/auth/auth.module.ts
@@ -4,9 +4,19 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { JwtStrategy } from './strategy/jwt.strategy';
 import { PrismaModule } from 'src/prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [PrismaModule, JwtModule.register({ secret: 'secretKey', signOptions: { expiresIn: '1m' } })],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    PrismaModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET_KEY,
+      signOptions: { expiresIn: '1m' },
+    }),
+  ],
   providers: [AuthService, JwtStrategy],
   controllers: [AuthController],
   exports: [JwtModule, AuthService],
